420c972ab5a743bced7660a44ad2b1a53a92fd9a
brunoAE120 <brunnoribeirinho@hotmail.com>
2021-12-04 21:49:33 -0300

    Correção AF2 2

--- src/App.vue
+++ src/App.vue
@@ -1,214 +1,32 @@
 <template>
-  <v-app>
-    <v-toolbar 
-    color="orange"
-    dense
-    elevation="4"
-    outlined
-    shaped>
-    <v-overflow-btn
-      :items="dropdown_font"
-      label="Select font"
-      hide-details
-      class="pa-0"
-    ></v-overflow-btn>
-
-    <template v-if="$vuetify.breakpoint.mdAndUp">
-      <v-divider vertical></v-divider>
-
-      <v-overflow-btn
-        :items="dropdown_edit"
-        editable
-        label="Select size"
-        hide-details
-        class="pa-0"
-        overflow
-      ></v-overflow-btn>
-
-      <v-divider vertical></v-divider>
-
-      <v-spacer></v-spacer>
-
-      <v-btn-toggle
-        v-model="toggle_multiple"
-        color="primary"
-        dense
-        group
-        multiple
-      >
-        <v-btn
-          :value="1"
-          text
-        >
-          <v-icon>mdi-format-bold</v-icon>
-        </v-btn>
-
-        <v-btn
-          :value="2"
-          text
-        >
-          <v-icon>mdi-format-italic</v-icon>
-        </v-btn>
-
-        <v-btn
-          :value="3"
-          text
-        >
-          <v-icon>mdi-format-underline</v-icon>
-        </v-btn>
-
-        <v-btn
-          :value="4"
-          text
-        >
-          <v-icon>mdi-format-color-fill</v-icon>
-        </v-btn>
-      </v-btn-toggle>
-
-      <div class="mx-4"></div>
-
-      <v-btn-toggle
-        v-model="toggle_exclusive"
-        color="primary"
-        dense
-        group
-      >
-        <v-btn
-          :value="1"
-          text
-        >
-          <v-icon>mdi-format-align-left</v-icon>
-        </v-btn>
-
-        <v-btn
-          :value="2"
-          text
-        >
-          <v-icon>mdi-format-align-center</v-icon>
-        </v-btn>
-
-        <v-btn
-          :value="3"
-          text
-        >
-          <v-icon>mdi-format-align-right</v-icon>
-        </v-btn>
-
-        <v-btn
-          :value="4"
-          text
-        >
-          <v-icon>mdi-format-align-justify</v-icon>
-        </v-btn>
-      </v-btn-toggle>
-    </template>
-  </v-toolbar>
-
-    <v-navigation-drawer
-    v-model="drawer"
-      absolute>
-      <v-list-item>
-
-        <v-list-item-content>
-          <v-list-item-title>Bruno Almeida</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-       <v-btn-toggle
-        v-model="toggle_exclusive"
-        color="primary"
-        dense
-        group
-      >
-        <v-btn
-          :value="1"
-          text
-        >
-          <v-icon>mdi-format-align-left</v-icon>
-        </v-btn>
-
-        <v-btn
-          :value="2"
-          text
-        >
-          <v-icon>mdi-format-align-center</v-icon>
-        </v-btn>
-
-        <v-btn
-          :value="3"
-          text
-        >
-          <v-icon>mdi-format-align-right</v-icon>
-        </v-btn>
-
-        <v-btn
-          :value="4"
-          text
-        >
-          <v-icon>mdi-format-align-justify</v-icon>
-        </v-btn>
-      </v-btn-toggle>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-  </v-navigation-drawer>
-
-    <v-app-bar
-    >
-      
-      <v-spacer></v-spacer>
-
-      
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-    </v-app-bar>
-
-    <v-main>
-      <Footer/>
-      <BottomNavBar/>
-      <NavBar/>
-      <StatusBar/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import BottomNavBar from './components/BottomNavBar.vue';
-import Footer from './components/Footer.vue';
-import NavBar from './components/NavBar.vue';
-import StatusBar from './components/StatusBar.vue';
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
+#nav {
+  padding: 30px;
 
-  components: {
-    Footer,
-    BottomNavBar,
-    NavBar,
-    StatusBar
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-
-  },
-
-  data: () => ({
-
-    
-    
-  }),
-};
-</script>
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
